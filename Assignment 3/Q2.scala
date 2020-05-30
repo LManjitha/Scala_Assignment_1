fcdc94a78409986deed888daa77ef92399b6d346
LManjitha <56678587+LManjitha@users.noreply.github.com>
2020-05-30 14:44:25 +0530

    Update Q2.scala

--- Assignment 3/Q2.scala
+++ Assignment 3/Q2.scala
@@ -1,9 +1,9 @@
 object Q2 extends  App{
   def prime(x:Int,n:Int=2):Int = x match {
-    case x if(x==2) => x
-    case x if(x%n==0) => prime(x-1)
-    case x if(n*n>x) => {
-      println(x)
+    case x if(x-1==2) => x-1
+    case x if((x-1)%n==0) => prime(x-1)
+    case x if(n*n>x-1) => {
+      println(x-1)
       prime(x-1)
     }
     case _ => prime(x,n+1)
