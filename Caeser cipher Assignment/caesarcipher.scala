d285e8bc39ec0be04ea7992f9adef759bf816e0e
LManjitha <56678587+LManjitha@users.noreply.github.com>
2020-07-19 11:51:00 +0530

    Update caesarcipher.scala

--- Caeser cipher Assignment/caesarcipher.scala
+++ Caeser cipher Assignment/caesarcipher.scala
@@ -1,14 +1,16 @@
 object caesarcipher extends App{
 
-  println("Enter Sting: ")
+  print("Enter Sting: ")
   val s=scala.io.StdIn.readLine()
+  print("Enter number of shifts: ")
+  val shifts=scala.io.StdIn.readInt()
   val alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   val encrypt=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)+key)%a.size)
   val decrypt=(c:Char,key:Int,a:String)=>a((a.indexOf(c.toUpper)-key+a.size)%a.size)
   val cipher=(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
 
-  val ct=cipher(encrypt,s,5,alphabet)
-  val cp=cipher(decrypt,ct,5,alphabet)
+  val ct=cipher(encrypt,s,shifts,alphabet)
+  val cp=cipher(decrypt,ct,shifts,alphabet)
 
   println("After Encrypt the string : "+ct)
   println("After Decrypt the encrypted string : "+cp)
