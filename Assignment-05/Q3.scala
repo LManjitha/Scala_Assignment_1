aa21923396e8d69b8fee45dc6a517ed32366dabb
LManjitha <56678587+LManjitha@users.noreply.github.com>
2020-07-29 23:25:56 +0530

    Update Q3.scala

--- Assignment-05/Q3.scala
+++ Assignment-05/Q3.scala
@@ -1,7 +1,7 @@
 object Q3 extends App{
-  var a1=new account("1",111,1000)
-  var a2=new account("2",222,1500)
-  var a3=new account("3",333,2000)
+  var a1=new account("983101664V",111,1000)
+  var a2=new account("983151898V",222,1500)
+  var a3=new account("983184565V",333,2000)
   a1.transfer(a2,500)
   println(a1)
   println(a2)
